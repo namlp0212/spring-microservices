2f373001687b522e080811a83cf93ce57239b662
namlp0212 <namlp0212@gmail.com>
2024-10-23 15:18:11 +0700

    config eureka server

---
+++ eureka-serer/src/test/java/com/example/eureka_serer/EurekaSererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_serer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
